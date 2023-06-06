146d8df949fe688a55b08c231b306a63ac764d89
RaviShankar <ravishankar@Ravi-MacBook-Air.local>
2023-06-06 16:33:27 +0530

    update main

--- provider.tf
+++ provider.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      source = "hashicorp/azurerm"
+      version = "3.59.0"
     }
   }
 }
-
 provider "azurerm" {
   skip_provider_registration = true
   features {}
