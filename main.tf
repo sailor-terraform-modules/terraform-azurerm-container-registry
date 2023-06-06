f24c63d933f74189634d1adb732b041c5778a877
RaviShankar <ravishankar@Ravi-MacBook-Air.local>
2023-06-06 16:04:25 +0530

    inital commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container_registry" "acr" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  sku                 = var.sku
+  admin_enabled       = false
+}
